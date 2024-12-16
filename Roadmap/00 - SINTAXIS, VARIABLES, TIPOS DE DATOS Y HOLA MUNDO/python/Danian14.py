1e0ddf8da3bc43ca97379f6a75cee3d564b348fe
Danian14 <daniancontrerassalazar@gmail.com>
2024-12-16 01:13:56 +0100

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Danian14.py
@@ -0,0 +1,30 @@
+# Web oficial de Python: https://www.python.org/
+
+#El comentario de una linea se crea usando un numeral(#) al inicio de la linea.
+
+"""
+Esto es un comentario
+de varias
+lineas, usando comillas dobles.
+"""
+
+
+'''
+Esto es un comentario 
+de varias lineas 
+usando comillas simples.
+'''
+
+x = 20  #Esto es una variable.
+
+MI_CONSTANTE = 15  #las constantes en python no existen pero se puede crear una variable y 
+         #dejarle un comentario indicando que "es una constante".
+
+#Tipos de datos primitivos: 
+str = "palabra"
+int = 20
+bool = False
+bool = True
+float = 3.5
+
+print("¡Hola, Python")
